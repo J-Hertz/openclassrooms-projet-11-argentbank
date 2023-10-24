0933a6ac7e88846e58741cd65ff62b3cc260c5fa
Jonathan Hertz <john.ztreh@gmail.com>
2023-10-24 14:19:55 +0200

    added SignInPage and modified routes

--- argentbank/src/components/Header/Header.js
+++ argentbank/src/components/Header/Header.js
@@ -2,23 +2,24 @@ import argentBankLogo from '../../assets/img/argentBankLogo.png';
 import './header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
           src={argentBankLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in">
+        <Link to="/sign-in" className="main-nav-item">
           <FontAwesomeIcon icon={faUserCircle} />
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   );
--- argentbank/src/router/AppRouter.js
+++ argentbank/src/router/AppRouter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
-import SignIn from '../pages/SignIn/SignIn';
+import SignInPage from '../pages/SignInPage/SignInPage';
 import User from '../pages/User/User';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-in" element={<SignInPage />} />
       <Route path="/user" element={<User />} />
     </Routes>
   );
